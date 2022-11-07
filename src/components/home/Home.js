a4d6ddf21ba534281847d8c01e66429750cb74c2
King-Mikaelson <anazodomichael27@gmail.com>
2022-11-07 23:45:46 +0100

    Add changes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function Home() {
   return (
-    <div style={{display:"flex", justifyContent:"center", alignItems:"center", background:"black", color:"whitesmoke", height:"100vh"}}>
+    <div style={{display:"flex", justifyContent:"center", alignItems:"center", background:"black", color:"whitesmoke", height:"100vh", textAlign:'center'}}>
         <h1>Welcome To Alucard GitHub Repo</h1>
     </div>
   )
